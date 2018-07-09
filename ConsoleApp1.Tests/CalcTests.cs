895994c59f3b082ee80a7f6afe855b67371fafe1
Wojciech Regulski <invalidcorgi@gmail.com>
2018-07-09 23:15:32 +0200

    cos tam na start

---
+++ ConsoleApp1.Tests/CalcTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApp1.Tests
+{
+    public class CalcTests
+    {
+        [Test]
+        public void AddPositives()
+        {
+            Assert.AreEqual(4, Calc.Add(2, 2));
+        }
+
+        [Test]
+        public void AddNegatives()
+        {
+            Assert.AreEqual(-6, Calc.Add(-2, -4));
+        }
+    }
+}
